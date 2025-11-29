a329a2bb81d82b6c0eb199f949a8f5adc3f4ba52
arafa0001 <abdulrafayimran4@gmail.com>
2025-11-30 00:20:57 +0500

    designing hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import Nav from "../components/homeComponents/Nav.jsx"
-import video from "../assets/bg-video.mp4"
+import React from "react";
+import Nav from "../components/homeComponents/Nav.jsx";
+import video from "../assets/bg-video.mp4";
 
 const Home = () => {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-
-      
-      <video 
+      <video
         className="w-full h-full object-cover top-0 left-0 -z-10 fixed  "
-        autoPlay 
-        muted 
-        loop 
+        autoPlay
+        muted
+        loop
         src={video}
       ></video>
 
-      {/* Page Content */}
-      <h1 className="text-white text-5xl font-bold mt-20 ml-10">
-        Home
-      </h1>
-
+      <div className="font-[font300] text-[9vw] text-white uppercase leading-[9vw] flex items-center flex-col">
+        <div>L'étincelle </div>
+        <div>
+          qui
+          <div className="w-[16vw] h-[7vw] inline-block overflow-hidden rounded-full">
+            <video
+              className="w-full  h-full object-cover"
+              autoPlay
+              muted
+              loop
+              src={video}
+            ></video>
+          </div>
+          génère 
+        </div>
+        <div>la créativité</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
