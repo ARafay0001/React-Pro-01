45b9717fab183eb071c442bee138c6ab4c161b48
arafa0001 <abdulrafayimran4@gmail.com>
2025-12-04 02:34:33 +0500

    images animation

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,27 +1,111 @@
-import React from 'react'
-import Nav from "../components/homeComponents/Nav.jsx"
+import React, { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import img01 from "../assets/Images/01.png";
+import img02 from "../assets/Images/02.png";
+import img03 from "../assets/Images/03.png";
+import img04 from "../assets/Images/04.png";
+import img05 from "../assets/Images/05.png";
+import img06 from "../assets/Images/06.png";
+import img07 from "../assets/Images/07.png";
+import img08 from "../assets/Images/08.png";
+import img09 from "../assets/Images/09.png";
+import img10 from "../assets/Images/10.png";
+
 const Agence = () => {
+  const imgDivRef = useRef(null);
+  const imgRef = useRef(null);
 
+  const imgArray = [
+    img01,
+    img02,
+    img03,
+    img04,
+    img05,
+    img06,
+    img07,
+    img08,
+    img09,
+    img10
+  ];
 
+  gsap.registerPlugin(ScrollTrigger);
 
+  useGSAP(() => {
+    gsap.to(imgDivRef.current, {
+      scrollTrigger: {
+        trigger: imgDivRef.current,
+        start: "top 23%",
+        end: "top -100%",
+        scrub: true,
+        markers: true,
+        pin: true,
+        onUpdate: (self) => {
+          let rawIndex = self.progress * imgArray.length;
+          let index = Math.floor(rawIndex);
 
-  
+          // Keep index inside valid range
+          if (index >= imgArray.length) index = imgArray.length - 1;
+          if (index < 0) index = 0;
+
+          // Your custom logic: last 5 images after reaching end
+          let finalIndex = index;
+ 
+          if (self.progress >= 1) {
+            finalIndex = imgArray.length - 5;
+          }
+
+          imgRef.current.src = imgArray[finalIndex];
+          // const random = Math.floor(Math.random() * (imgArray.length - 1))
+          // console.log(random)
+          // imgRef.current.src = imgArray[random];
+
+          
+        },
+      },
+    });
+  }, []);
+
   return (
     <div>
-    <div className='w-[15vw] object-cover rounded-[1.5vw] overflow-hidden absolute top-[10vw] left-[30vw]'>
-        <img className='w-full' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+      <div
+        ref={imgDivRef}
+        className="w-[15vw] h-[19vw] rounded-[1.5vw] overflow-hidden absolute top-38 left-[30%]"
+      >
+        <img
+          ref={imgRef}
+          className="w-full h-full object-cover"
+          src={imgArray[0]}
+          alt=""
+        />
       </div>
-      <div className='relative'>
-        <div className='text-[20vw] font-[font500] text-center leading-[18vw] mt-[25%] uppercase '> 
-        Soixan7e
-        Douze
-      </div>
-      <div className='flex justify-end'>
-        <div className='text-[4vw] leading-[4vw] font-[font500] w-[50%]'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</div>
-      </div>
-      </div>
-    </div>
-  )
-}
 
-export default Agence
+      <div className="relative">
+        <div className="text-[20vw] font-[font500] text-center leading-[18vw] mt-[25%] uppercase">
+          Soixan7e Douze
+        </div>
+
+        <div className="flex justify-end">
+          <div className="text-6xl font-[font500] w-[50%]">
+            Notre curiosité nourrit notre créativité. On reste humbles et on dit
+            non aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+            valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+            faire de bons chiffres à court terme, mais on la tue à long terme.
+            C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir
+            des marques influentes.
+          </div>
+        </div>
+      </div>
+
+      {/* Preload images
+      <div style={{ display: "none" }}>
+        {imgArray.map((img, i) => (
+          <img key={i} src={img} alt="" />
+        ))}
+      </div> */}
+    </div>
+  );
+};
+
+export default Agence;
