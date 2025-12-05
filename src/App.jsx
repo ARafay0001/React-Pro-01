f0729c8c47cca2ed9d850ceff52f576237e15411
arafa0001 <abdulrafayimran4@gmail.com>
2025-12-06 01:36:27 +0500

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import Home from "./pages/Home.jsx" 
 import Project from "./pages/Project.jsx" 
 import Agence from "./pages/Agence.jsx" 
+import NavBar from './components/NavBar.jsx'
+import FullNav from './components/FullNav.jsx'
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
 
       <div>
       
-
+      <NavBar />
+      <FullNav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
